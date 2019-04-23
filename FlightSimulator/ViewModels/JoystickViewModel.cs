46a1dee69f20c256fd1d315d670dba7d04353e69
Shani <shani_z2007@walla.com>
2019-04-23 16:34:05 +0300

    throttle maybe work

---
+++ FlightSimulator/ViewModels/JoystickViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightSimulator.Model;
+
+namespace FlightSimulator.ViewModels
+{
+    class JoystickViewModel : BaseNotify
+    {
+        private float changeThrottle;
+        private float changeRudder;
+        private float changeAileron;
+        private float changeElevator;
+
+        // properties
+        public float ChangeThrottle
+        {
+            get
+            {
+                return this.changeThrottle;
+            }
+            set
+            {
+                this.changeThrottle = value;
+                sliderChanges("controls/engines/current-engine/throttle", changeThrottle);
+                NotifyPropertyChanged("changeThrottle");
+            }
+        }
+
+        private void sliderChanges(string pathName, float val)
+        {
+            string strVal = val.ToString("0.00");
+            string command = "set " + pathName + " " + strVal;
+            Command.Instance.sendToSimulator(command);
+
+        }
+    }
+}
