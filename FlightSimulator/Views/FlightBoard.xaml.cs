44747e19e4e915d4bdfd925822d80b9c9e11603b
Shani <shani_z2007@walla.com>
2019-04-28 10:23:08 +0300

    final changes

--- FlightSimulator/Views/FlightBoard.xaml.cs
+++ FlightSimulator/Views/FlightBoard.xaml.cs
@@ -52,7 +52,8 @@ namespace FlightSimulator.Views
         {
             if(e.PropertyName.Equals("Lat") || e.PropertyName.Equals("Lon"))
             {
-                Point p1 = new Point(FlightBoardViewModel.Instance.Lat, FlightBoardViewModel.Instance.Lon);            // Fill here!
+                // Fill here!
+                Point p1 = new Point(FlightBoardViewModel.Instance.Lat, FlightBoardViewModel.Instance.Lon);
                 planeLocations.AppendAsync(Dispatcher, p1);
             }
         }
